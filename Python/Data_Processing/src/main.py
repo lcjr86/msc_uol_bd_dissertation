05b67d44f90a21d7349585a0292544ad3d3ad808
Luiz Carlos de Jesus Junior <luiz.jesus@ricev.io>
2019-11-14 09:06:32 +0100

    few changes data processing main

--- Python/Data_Processing/src/main.py
+++ Python/Data_Processing/src/main.py
@@ -42,31 +42,39 @@ if __name__ == "__main__":
         kafka_topic_structure = 'sensor.[SENSOR_ID]'
         print("kafka_topic_structure|" + kafka_topic_structure)
 
+        ssc.start()
+        print("ssc started")
+
+        ssc.awaitTermination()
+        print("ssc awaitTermination")
+
         for i in range(0, int(number_topics)):
 
-            ssc.start()
+            print("***** " + str(i) + " *****")
 
             topic = kafka_topic_structure.replace("[SENSOR_ID]", str(i + 1))
             print("topic|" + topic)
 
             folder_topic = topic.replace(".", "_")
+            print("folder_topic|" + folder_topic)
 
             ### Returns DStream
             kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": broker})
+            print("kvs|" + str(kvs))
 
             ### DStream Twith the 2nd element of the tuple
             sensor_message = kvs.map(lambda x: x[1])
+            print("sensor_message|" + str(sensor_message))
 
             ### Filtering only the even numbers
             sensor_message_filtered = sensor_message.filter(lambda x: float(x.replace(re.findall('^[0-9]{4}-[0-9]{2}-[0-9]{2}\|[0-9]{2}\:[0-9]{2}:[0-9]{2}\|', x)[0], "")) % 2 == 0)
+            print("sensor_message_filtered|" + str(sensor_message_filtered))
 
             ### Print the messages
             sensor_message_filtered.pprint()
 
             ### Export to MongoDB
             sensor_message_filtered.foreachRDD(lambda x: sendToMongoDB(x))
-
-            ssc.stop()
         
     except Exception as e:
         print(e)
