b5016eab864103f72c4daff776c933e82c0ad79a
Luiz Carlos de Jesus Junior <luiz.jesus@ricev.io>
2019-11-14 15:34:03 +0100

    new main

--- Python/Data_Processing/src/main.py
+++ Python/Data_Processing/src/main.py
@@ -9,9 +9,36 @@ from pyspark import SparkConf
 
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    
-    try:
+class ConnectAndFilter():
+
+    def __init__(self, kafka_topic_structure, ssc, broker):
+        self.kafka_topic_structure = kafka_topic_structure
+        self.ssc = ssc
+        self.broker = broker
+
+    def run(self):
+
+        topic = self.kafka_topic_structure.replace("[SENSOR_ID]", str(i + 1))
+        print("topic|" + topic)
+
+        folder_topic = topic.replace(".", "_")
+        print("folder_topic|" + folder_topic)
+
+        ### Returns DStream
+        kvs = KafkaUtils.createDirectStream(self.ssc, [topic], {"metadata.broker.list": self.broker})
+        print("kvs|" + str(kvs))
+
+        ### DStream Twith the 2nd element of the tuple
+        sensor_message = kvs.map(lambda x: x[1])
+        print("sensor_message|" + str(sensor_message))
+
+        ### Filtering only the even numbers
+        sensor_message_filtered = sensor_message.filter(lambda x: float(
+            x.replace(re.findall('^[0-9]{4}-[0-9]{2}-[0-9]{2}\|[0-9]{2}\:[0-9]{2}:[0-9]{2}\|', x)[0], "")) % 2 == 0)
+        print("sensor_message_filtered|" + str(sensor_message_filtered))
+
+        ### Print the messages
+        sensor_message_filtered.pprint()
 
         def sendToMongoDB(rdd):
             if not rdd.isEmpty():
@@ -22,6 +49,13 @@ if __name__ == "__main__":
                 collection = db[folder_topic]
                 data = collection.insert_one({'date': message[0], 'time': message[1], 'value': message[2]})
                 client.close()
+
+        ### Export to MongoDB
+        sensor_message_filtered.foreachRDD(lambda x: sendToMongoDB(x))
+
+if __name__ == "__main__":
+    
+    try:
 
         number_topics = sys.argv[1]
 
@@ -34,47 +68,30 @@ if __name__ == "__main__":
         kafka_topic_structure = 'sensor.[SENSOR_ID]'
         print("kafka_topic_structure|" + kafka_topic_structure)
 
+        conf = SparkConf()
+        conf.setMaster('yarn')
+        conf.setAppName('ProcessKafkaSensorData')
+
+        sc = SparkContext(conf=conf)
+        spark = SparkSession(sc)
+        ssc = StreamingContext(sc, 1)
+
+        list_connect_and_run = []
+
         for i in range(0, int(number_topics)):
 
             print("***** " + str(i) + " *****")
+            list_connect_and_run.append(ConnectAndFilter(kafka_topic_structure, ssc, broker))
+            print("list_connect_and_run[" + str(i + 1) + "] instantiated")
 
-            conf = SparkConf()
-            conf.setMaster('yarn')
-            conf.setAppName('ProcessKafkaSensorData')
+        for i in range(0, int(number_topics)):
 
-            sc = SparkContext(conf=conf)
-            spark = SparkSession(sc)
-            ssc = StreamingContext(sc, 1)
+            print("***** " + str(i) + " *****")
+            list_connect_and_run[i].run()
+            print("list_connect_and_run[" + str(i + 1) + "].run() executed")
 
-            ssc.start()
-            print("ssc.start()")
-
-            topic = kafka_topic_structure.replace("[SENSOR_ID]", str(i + 1))
-            print("topic|" + topic)
-
-            folder_topic = topic.replace(".", "_")
-            print("folder_topic|" + folder_topic)
-
-            ### Returns DStream
-            kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": broker})
-            print("kvs|" + str(kvs))
-
-            ### DStream Twith the 2nd element of the tuple
-            sensor_message = kvs.map(lambda x: x[1])
-            print("sensor_message|" + str(sensor_message))
-
-            ### Filtering only the even numbers
-            sensor_message_filtered = sensor_message.filter(lambda x: float(x.replace(re.findall('^[0-9]{4}-[0-9]{2}-[0-9]{2}\|[0-9]{2}\:[0-9]{2}:[0-9]{2}\|', x)[0], "")) % 2 == 0)
-            print("sensor_message_filtered|" + str(sensor_message_filtered))
-
-            ### Print the messages
-            sensor_message_filtered.pprint()
-
-            ### Export to MongoDB
-            sensor_message_filtered.foreachRDD(lambda x: sendToMongoDB(x))
-
-            ssc.stop()
-            print("ssc.stop()")
+        ssc.start()
+        ssc.awaitTermination()
 
     except Exception as e:
         print(e)
