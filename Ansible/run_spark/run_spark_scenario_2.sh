beb3957f2cec73958eeca3abd0f57d74d073a54c
Luiz Carlos de Jesus Junior <luiz.jesus@ricev.io>
2019-11-16 23:01:39 +0100

    new scenario 1-2-3

---
+++ Ansible/run_spark/run_spark_scenario_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/hadoop/spark/bin
+#nohup ./spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 --master yarn --deploy-mode cluster /home/pi/Repositories/msc_uol_dissertation/Python/Data_Processing/src/main.py 2 &
+nohup spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 --master yarn --deploy-mode cluster /home/pi/Repositories/msc_uol_dissertation/Python/Data_Processing/src/main.py 2 &
--- Ansible/virtualMachine/scripts/run_ansible_example.sh
+++ Ansible/virtualMachine/scripts/run_ansible_example.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
 
+## My Personal Mac
 
-### My Professional Mac
-
-#### Cluster
-ansible-playbook -i /Users/luizcarlosdejesusjunior/Documents/Repos/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini \
-/Users/luizcarlosdejesusjunior/Documents/Repos/msc_uol_dissertation/Ansible/virtualMachine/scripts/00_setup_cluster_rasbian.yaml \
---user=root \
---ask-become-pass
-
-### My Personal Mac
-
-#### Set Cluster
+### Set Cluster
 ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/00_setup_cluster_rasbian.yaml' --user=root --ask-become-pass
 
 #### start Local
 ansible-playbook '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/host/scripts/00_start_host_services.yaml' --user=root --ask-become-pass
 
 
+### scenario_1
 
-#### scenario_1
+ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_1__00_num_sensor_1.yaml'  --user=root --ask-become-pass
 
-ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_1_num_sensor_1.yaml'  --user=root --ask-become-pass
-
-##### Run MQTT_Kafka_bridge
-ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_1__run_mqtt_kafka_bridge.yaml'  --user=root --ask-become-pass
-
-##### Run PySpark script
-ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_1__run_pyspark_script.yaml'  --user=root --ask-become-pass
+##### Run 'sensor_emulator'
+ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_1__50_run_sensor_emulator.yaml'  --user=root --ask-become-pass
 
 
 #### scenario_2
 
-ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_2_num_sensor_2.yaml'  --user=root --ask-become-pass
+ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_2__00_num_sensor_2.yaml'  --user=root --ask-become-pass
 
+##### Run 'sensor_emulator'
+ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_2__50_run_sensor_emulator.yaml'  --user=root --ask-become-pass
 
 
 #### scenario_3
 
-ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_3_num_sensor_5.yaml'  --user=root --ask-become-pass
+ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_3__00_num_sensor_4.yaml'  --user=root --ask-become-pass
+
+##### Run 'sensor_emulator'
+ansible-playbook -i '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/inventory_raspbian.ini' '/Users/lcjr86/Dropbox/MSc - Liverpool/CKIT-702--Computing Advisor Class/Implementation/msc_uol_dissertation/Ansible/virtualMachine/scripts/scenario_3__50_run_sensor_emulator.yaml'  --user=root --ask-become-pass
