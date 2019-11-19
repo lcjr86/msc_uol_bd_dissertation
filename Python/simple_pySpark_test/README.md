c0894c9fe7e2e76864f365ebd295ee89012a8baa
Luiz Carlos de Jesus Junior <luiz.jesus@ricev.io>
2019-11-19 15:49:29 +0100

    documentation

--- Python/simple_pySpark_test/README.md
+++ Python/simple_pySpark_test/README.md
@@ -1,1 +1,11 @@
-Example take from: https://medium.com/@o20021106/run-pyspark-on-yarn-c7cd04b87d81
+# simple_pyspark_test
+
+## /Python/simple_pyspark_test/src/
+
+### test_pyspark_yarn.py
+
+Run a simple spark process using Yarn
+
+## References
+
+https://medium.com/@o20021106/run-pyspark-on-yarn-c7cd04b87d81
--- Python/simple_pySpark_test/src/test_pyspark_yarn.py
+++ Python/simple_pySpark_test/src/test_pyspark_yarn.py
@@ -1,13 +1,26 @@
+### Import necessary libraries (from pyspar)
 from pyspark import SparkConf
 from pyspark import SparkContext
 
+
+### Create the Spark Configuration Object
 conf = SparkConf()
+
+### Set on the Spark Configuration 'YARN" as framework used to process the job
 conf.setMaster('yarn')
+
+### Set the Application Name
 conf.setAppName('testing')
+
+### Create the Spark Context based on the configuration define previously
 sc = SparkContext(conf=conf)
 
+### Run the parallization
 rdd = sc.parallelize([1,2,3])
+
+### Make the counting
 count = rdd.count()
 
+### Print the results
 print(sc.master)
 print(count) 
